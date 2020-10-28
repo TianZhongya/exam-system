69c81bcfa791adf9b3eaf8c8fd5890479aaf6863
TianZY <1438472761@qq.com>
2020-10-28 22:40:03 +0800

    用户管理完善

--- src/main/java/com/example/tzy/demo/http/controller/UserController.java
+++ src/main/java/com/example/tzy/demo/http/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.example.tzy.demo.http.controller;
 
+import com.example.tzy.demo.biz.domain.bto.BatchGet;
+import com.example.tzy.demo.biz.domain.bto.request.UserQueryRequest;
+import com.example.tzy.demo.biz.domain.bto.request.UserRegisterRequest;
+import com.example.tzy.demo.biz.domain.bto.response.CoreUserInfo;
 import com.example.tzy.demo.biz.service.UserManageService;
+import com.example.tzy.demo.http.common.Response;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: Tianzy
@@ -18,5 +23,22 @@ public class UserController {
 
     public UserController(UserManageService userManageService) {
         this.userManageService = userManageService;
+    }
+
+    @ApiOperation(value="注册用户",notes="需要邀请码")
+    @PostMapping
+    public Response<Long> registerUser(@RequestBody @Validated UserRegisterRequest registerRequest){
+        return Response.success(userManageService.registerUser(registerRequest));
+    }
+
+    @ApiOperation(value="查找用户", notes = "支持模糊搜索")
+    public Response<BatchGet<CoreUserInfo>> find(UserQueryRequest request){
+        return Response.success(userManageService.query(request));
+    }
+
+    @ApiOperation(value = "删除用户")
+    @DeleteMapping("/{id}")
+    public Response<Boolean> delete(@PathVariable Long id){
+        return Response.success(userManageService.delete(id));
     }
 }
